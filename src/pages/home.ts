d65629c0eff7b9316b920b8fd06361a8ff4d3ed4
g4knr <85592362+g4knr@users.noreply.github.com>
2023-05-25 11:39:50 +0100

    Removed header

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -7,57 +7,55 @@ export const home = () => {
 
   gsap.registerPlugin(ScrollTrigger, SplitText);
 
-  //   ScrollSmoother.create({ smooth: 1 });
+  // const header = document.querySelector<HTMLHeadElement>('header');
+  // const list = document.querySelector<HTMLDivElement>('.projects_list');
+  // const images = list?.querySelectorAll<HTMLImageElement>('img');
+  // if (!header || !list || !images) return;
 
-  const header = document.querySelector<HTMLHeadElement>('header');
-  const list = document.querySelector<HTMLDivElement>('.projects_list');
-  const images = list?.querySelectorAll<HTMLImageElement>('img');
-  if (!header || !list || !images) return;
-
-  const headerHeight = list.offsetWidth;
-  const toMove = ((list.childElementCount - 1) / list.childElementCount) * 100;
-  header.style.height = `${headerHeight}px`;
+  // const headerHeight = list.offsetWidth;
+  // const toMove = ((list.childElementCount - 1) / list.childElementCount) * 100;
+  // header.style.height = `${headerHeight}px`;
 
   //   const proxy = { scale: 1 };
   //   const xSetter = gsap.utils.pipe(gsap.utils.clamp(1, 2), gsap.quickSetter('img', 'scale'));
 
-  const headerTl = gsap.timeline({
-    defaults: {
-      ease: 'none',
-    },
-    scrollTrigger: {
-      trigger: header,
-      start: 'top top',
-      end: 'bottom top',
-      scrub: 1,
-      //   onUpdate: (self) => {
-      //     const scale = gsap.utils.clamp(1, 2, self.getVelocity() / 10000);
-      //     // only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
-      //     if (Math.abs(scale) > Math.abs(proxy.scale)) {
-      //       proxy.scale = scale;
-      //       gsap.to(proxy, {
-      //         scale: 1,
-      //         duration: 0.8,
-      //         ease: 'power3',
-      //         overwrite: true,
-      //         onUpdate: () => xSetter(proxy.scale),
-      //       });
-      //     }
-      //   },
-    },
-  });
+  // const headerTl = gsap.timeline({
+  //   defaults: {
+  //     ease: 'none',
+  //   },
+  //   scrollTrigger: {
+  //     trigger: header,
+  //     start: 'top top',
+  //     end: 'bottom top',
+  //     scrub: 1,
+  //     //   onUpdate: (self) => {
+  //     //     const scale = gsap.utils.clamp(1, 2, self.getVelocity() / 10000);
+  //     //     // only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
+  //     //     if (Math.abs(scale) > Math.abs(proxy.scale)) {
+  //     //       proxy.scale = scale;
+  //     //       gsap.to(proxy, {
+  //     //         scale: 1,
+  //     //         duration: 0.8,
+  //     //         ease: 'power3',
+  //     //         overwrite: true,
+  //     //         onUpdate: () => xSetter(proxy.scale),
+  //     //       });
+  //     //     }
+  //     //   },
+  //   },
+  // });
 
-  headerTl
-    .to(list, {
-      xPercent: -toMove,
-    })
-    .to(
-      images,
-      {
-        xPercent: -50,
-      },
-      '<'
-    );
+  // headerTl
+  //   .to(list, {
+  //     xPercent: -toMove,
+  //   })
+  //   .to(
+  //     images,
+  //     {
+  //       xPercent: -50,
+  //     },
+  //     '<'
+  //   );
 
   const takeover = document.querySelector<HTMLDivElement>('.takeover');
   const takeoverContent = takeover?.querySelector<HTMLDivElement>('.takeover_content');
